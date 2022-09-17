28f554d5cbb11118b06961cf318499266ed9575a
ReeseJohnson22 <johnsonreese151@gmail.com>
2022-09-16 22:32:09 -0400

    Pages w/buttons denoting to next page
    All the pages set up for the code with buttons and labels, just need to link pages together.

---
+++ BaggageAutomation/StartFlight.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
+using static BaggageAutomation.SQL_Operations;
+using static BaggageAutomation.LuggageChecked;
+
+namespace BaggageAutomation
+{
+    public partial class StartFlight : Window
+    {
+        public StartFlight()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
